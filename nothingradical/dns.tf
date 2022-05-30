37e4d74db7a79db1510c406cd0f4483488211285
Frawley <frawleyskid@gmail.com>
2022-05-29 21:07:56 -0400

    Configure email routing for nothingradical

--- nothingradical/dns.tf
+++ nothingradical/dns.tf
@@ -13,3 +13,51 @@ resource "cloudflare_record" "www_aaaa" {
   value = "100::"
   proxied = true
 }
+
+resource "cloudflare_record" "apex_mx" {
+  for_each = {
+    route1 = {
+      value = "route1.mx.cloudflare.net"
+      priority = 44
+    }
+    route2 = {
+      value = "route2.mx.cloudflare.net"
+      priority = 63
+    }
+    route3 = {
+      value = "route3.mx.cloudflare.net"
+      priority = 28
+    }
+  }
+
+  zone_id = data.cloudflare_zone.nothingradical.id
+  type = "MX"
+  name = "@"
+  value = each.value.value
+  priority = each.value.priority
+  proxied = false
+}
+
+resource "cloudflare_record" "apex_txt_spf" {
+  zone_id = data.cloudflare_zone.nothingradical.id
+  type = "TXT"
+  name = "@"
+  value = "v=spf1 include:_spf.mx.cloudflare.net ~all"
+  proxied = false
+}
+
+resource "cloudflare_record" "txt_dmark" {
+  zone_id = data.cloudflare_zone.nothingradical.id
+  type = "TXT"
+  name = "_dmarc"
+  value = "v=DMARC1; p=reject; sp=reject; adkim=s; aspf=s;"
+  proxied = false
+}
+
+resource "cloudflare_record" "txt_domainkey" {
+  zone_id = data.cloudflare_zone.nothingradical.id
+  type = "TXT"
+  name = "*._domainkey"
+  value = "v=DKIM1; p="
+  proxied = false
+}
