2397fd0d4b97ff70a00b167f156ea9cdd5a55395
Frawley <frawleyskid@gmail.com>
2022-05-29 22:21:18 -0400

    Add zone settings to terraform config

--- acearchive/zone.tf
+++ acearchive/zone.tf
@@ -2,3 +2,42 @@ data "cloudflare_zone" "acearchive" {
   name = "acearchive.lgbt"
   account_id = var.cloudflare_account_id
 }
+
+resource "cloudflare_zone_dnssec" "acearchive" {
+  zone_id = data.cloudflare_zone.acearchive.id
+}
+
+resource "cloudflare_zone_settings_override" "acearchive" {
+  zone_id = data.cloudflare_zone.acearchive.id
+
+  settings {
+    always_use_https = "on"
+    automatic_https_rewrites = "on"
+    brotli = "on"
+    browser_check = "on"
+    early_hints = "on"
+    email_obfuscation = "on"
+    hotlink_protection = "off"
+    http3 = "on"
+    ip_geolocation = "on"
+    ipv6 = "on"
+    ssl = "strict"
+    security_level = "medium"
+    tls_1_3 = "zrt"
+    zero_rtt = "on"
+
+    minify {
+      html = "off"
+      css = "off"
+      js = "off"
+    }
+
+    security_header {
+      enabled = true
+      preload = true
+      max_age = 31536000
+      include_subdomains = true
+      nosniff = true
+    }
+  }
+}
--- nothingradical/zone.tf
+++ nothingradical/zone.tf
@@ -2,3 +2,42 @@ data "cloudflare_zone" "nothingradical" {
   name = "nothingradical.blog"
   account_id = var.cloudflare_account_id
 }
+
+resource "cloudflare_zone_dnssec" "nothingradical" {
+  zone_id = data.cloudflare_zone.nothingradical.id
+}
+
+resource "cloudflare_zone_settings_override" "nothingradical" {
+  zone_id = data.cloudflare_zone.nothingradical.id
+
+  settings {
+    always_use_https = "on"
+    automatic_https_rewrites = "on"
+    brotli = "on"
+    browser_check = "on"
+    early_hints = "on"
+    email_obfuscation = "on"
+    hotlink_protection = "off"
+    http3 = "on"
+    ip_geolocation = "on"
+    ipv6 = "on"
+    ssl = "strict"
+    security_level = "medium"
+    tls_1_3 = "zrt"
+    zero_rtt = "on"
+
+    minify {
+      html = "on"
+      css = "on"
+      js = "on"
+    }
+
+    security_header {
+      enabled = true
+      preload = true
+      max_age = 31536000
+      include_subdomains = true
+      nosniff = true
+    }
+  }
+}
