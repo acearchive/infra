4114d35a86dc8e5279a9a39c7b21f65eac5bc7e6
Frawley <frawleyskid@gmail.com>
2022-05-29 20:33:48 -0400

    Configure email routing via terraform

--- dns.tf
+++ dns.tf
@@ -22,13 +22,60 @@ resource "cloudflare_record" "acearchive_www_aaaa" {
   proxied = true
 }
 
+resource "cloudflare_record" "acearchive_apex_mx" {
+  for_each = {
+    route1 = {
+      value = "route1.mx.cloudflare.net"
+      priority = 85
+    }
+    route2 = {
+      value = "route2.mx.cloudflare.net"
+      priority = 93
+    }
+    route3 = {
+      value = "route3.mx.cloudflare.net"
+      priority = 87
+    }
+  }
+
+  zone_id = data.cloudflare_zone.acearchive.id
+  type = "MX"
+  name = "@"
+  value = each.value.value
+  priority = each.value.priority
+  proxied = false
+}
+
+resource "cloudflare_record" "acearchive_apex_txt_spf" {
+  zone_id = data.cloudflare_zone.acearchive.id
+  type = "TXT"
+  name = "@"
+  value = "v=spf1 include:_spf.mx.cloudflare.net ~all"
+  proxied = false
+}
+
+resource "cloudflare_record" "acearchive_txt_dmark" {
+  zone_id = data.cloudflare_zone.acearchive.id
+  type = "TXT"
+  name = "_dmarc"
+  value = "v=DMARC1; p=reject; sp=reject; adkim=s; aspf=s;"
+  proxied = false
+}
+
+resource "cloudflare_record" "acearchive_txt_domainkey" {
+  zone_id = data.cloudflare_zone.acearchive.id
+  type = "TXT"
+  name = "*._domainkey"
+  value = "v=DKIM1; p="
+  proxied = false
+}
+
 resource "cloudflare_record" "nothingradical_apex_cname" {
   zone_id = data.cloudflare_zone.nothingradical.id
   type = "CNAME"
   name = "@"
   value = "nothingradical-blog.pages.dev"
   proxied = true
-  allow_overwrite = true
 }
 
 resource "cloudflare_record" "nothingradical_www_aaaa" {
