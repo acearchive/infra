7a60a55d3a2cd330cf71ece12ee88f6c88b9e156
Frawley <frawley@duck.com>
2022-10-16 14:32:07 -0400

    Add ownership verification for SimpleLogin

--- acearchive/dns.tf
+++ acearchive/dns.tf
@@ -22,50 +22,10 @@ resource "cloudflare_record" "www_aaaa" {
   proxied = true
 }
 
-resource "cloudflare_record" "apex_mx" {
-  for_each = {
-    route1 = {
-      value = "route1.mx.cloudflare.net"
-      priority = 85
-    }
-    route2 = {
-      value = "route2.mx.cloudflare.net"
-      priority = 93
-    }
-    route3 = {
-      value = "route3.mx.cloudflare.net"
-      priority = 87
-    }
-  }
-
-  zone_id = data.cloudflare_zone.acearchive.id
-  type = "MX"
-  name = "@"
-  value = each.value.value
-  priority = each.value.priority
-  proxied = false
-}
-
-resource "cloudflare_record" "apex_txt_spf" {
+resource "cloudflare_record" "txt_sl_verification" {
   zone_id = data.cloudflare_zone.acearchive.id
   type = "TXT"
   name = "@"
-  value = "v=spf1 include:_spf.mx.cloudflare.net ~all"
-  proxied = false
-}
-
-resource "cloudflare_record" "txt_dmarc" {
-  zone_id = data.cloudflare_zone.acearchive.id
-  type = "TXT"
-  name = "_dmarc"
-  value = "v=DMARC1; p=reject; sp=reject; adkim=s; aspf=s;"
-  proxied = false
-}
-
-resource "cloudflare_record" "txt_domainkey" {
-  zone_id = data.cloudflare_zone.acearchive.id
-  type = "TXT"
-  name = "*._domainkey"
-  value = "v=DKIM1; p="
+  value = "sl-verification=fxncjuftchgummqcrwzrpmyucobgvg"
   proxied = false
 }
