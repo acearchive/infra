f94cebc28bb76ed66dbafd30f7ef171a7efcfd23
Frawley <frawley@duck.com>
2022-10-16 16:08:20 -0400

    Use SimpleLogin for nothingradical.blog

--- nothingradical/dns.tf
+++ nothingradical/dns.tf
@@ -14,19 +14,23 @@ resource "cloudflare_record" "www_aaaa" {
   proxied = true
 }
 
+resource "cloudflare_record" "apex_txt_sl_verification" {
+  zone_id = data.cloudflare_zone.nothingradical.id
+  type = "TXT"
+  name = "@"
+  value = "sl-verification=dqngxzimfmudfnhwdhaubcjzgiqlve"
+  proxied = false
+}
+
 resource "cloudflare_record" "apex_mx" {
   for_each = {
     route1 = {
-      value = "route1.mx.cloudflare.net"
-      priority = 44
+      value = "mx1.simplelogin.co."
+      priority = 10
     }
     route2 = {
-      value = "route2.mx.cloudflare.net"
-      priority = 63
-    }
-    route3 = {
-      value = "route3.mx.cloudflare.net"
-      priority = 28
+      value = "mx2.simplelogin.co."
+      priority = 20
     }
   }
 
@@ -42,22 +46,39 @@ resource "cloudflare_record" "apex_txt_spf" {
   zone_id = data.cloudflare_zone.nothingradical.id
   type = "TXT"
   name = "@"
-  value = "v=spf1 include:_spf.mx.cloudflare.net ~all"
+  value = "v=spf1 include:simplelogin.co ~all"
   proxied = false
 }
 
-resource "cloudflare_record" "txt_dmarc" {
+resource "cloudflare_record" "apex_cname_dkim" {
+  for_each = {
+    record1 = {
+      name = "dkim._domainkey"
+      value = "dkim._domainkey.simplelogin.co."
+    }
+
+    record2 = {
+      name = "dkim02._domainkey"
+      value = "dkim02._domainkey.simplelogin.co."
+    }
+
+    record3 = {
+      name = "dkim03._domainkey"
+      value = "dkim03._domainkey.simplelogin.co."
+    }
+  }
+
+  zone_id = data.cloudflare_zone.nothingradical.id
+  type = "CNAME"
+  name = each.value.name
+  value = each.value.value
+  proxied = false
+}
+
+resource "cloudflare_record" "apex_txt_dmarc" {
   zone_id = data.cloudflare_zone.nothingradical.id
   type = "TXT"
   name = "_dmarc"
-  value = "v=DMARC1; p=reject; sp=reject; adkim=s; aspf=s;"
-  proxied = false
-}
-
-resource "cloudflare_record" "txt_domainkey" {
-  zone_id = data.cloudflare_zone.nothingradical.id
-  type = "TXT"
-  name = "*._domainkey"
-  value = "v=DKIM1; p="
+  value = "v=DMARC1; p=quarantine; pct=100; adkim=s; aspf=s"
   proxied = false
 }
