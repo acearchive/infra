ac331863de988f69567b1f8d9c9886232225246e
Frawley <frawley@duck.com>
2022-11-30 23:14:17 -0500

    Add custom error response when rate limiting

--- acearchive/firewall.tf
+++ acearchive/firewall.tf
@@ -1,6 +1,6 @@
 resource "cloudflare_ruleset" "rate_limiting" {
   zone_id = data.cloudflare_zone.acearchive.id
-  name = "Rate-Limiting"
+  name = "rate_limiting"
   kind = "zone"
   phase = "http_ratelimit"
 
@@ -14,12 +14,29 @@ resource "cloudflare_ruleset" "rate_limiting" {
       ]
 
       period = 10
-      requests_per_period = 100
+      requests_per_period = 20
       mitigation_timeout = 10
     }
 
+    action_parameters {
+      response {
+        status_code = 429
+        content_type = "application/json"
+        content = jsonencode(local.rate_limit_response_body)
+      }
+    }
+
     expression = "not cf.bot_management.verified_bot"
-    description = "API Rate-Limiting"
+    description = "Rate-Limiting"
     enabled = true
+  }
+}
+
+locals {
+  rate_limit_response_body = {
+    type = "/problems/too-many-requests"
+    title = "Too Many Requests"
+    status = 429
+    detail = "You are being temporarily rate-limited."
   }
 }
