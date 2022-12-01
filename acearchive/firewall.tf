f8ceef1c0c628417a917c89501d1153c23ce4247
Frawley <frawley@duck.com>
2022-12-01 15:43:50 -0500

    Add comments explaining error response

--- acearchive/firewall.tf
+++ acearchive/firewall.tf
@@ -20,8 +20,11 @@ resource "cloudflare_ruleset" "rate_limiting" {
 
     action_parameters {
       response {
-        status_code = 429
+        # The Content-Type here *should* be `application/problem+json`, but
+        # Cloudflare only allows a small subset of media types for error
+        # response pages.
         content_type = "application/json"
+        status_code = 429
         content = jsonencode(local.rate_limit_response_body)
       }
     }
@@ -33,6 +36,9 @@ resource "cloudflare_ruleset" "rate_limiting" {
 }
 
 locals {
+  # The API returns `application/problem+json` response objects for errors. This
+  # error is being returned by the CDN, but we still want it to follow the same
+  # schema.
   rate_limit_response_body = {
     type = "/problems/too-many-requests"
     title = "Too Many Requests"
