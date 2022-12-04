30e91a8a0b324ce4bf6d91fc0ddfe5033234cc40
Frawley <frawley@duck.com>
2022-12-04 02:06:48 -0500

    Return strong etags for API and artifact files

---
+++ acearchive/cache.tf
@@ -0,0 +1,18 @@
+resource "cloudflare_ruleset" "etags" {
+  zone_id = data.cloudflare_zone.acearchive.id
+  name = "strong_etags"
+  kind = "zone"
+  phase = "http_request_cache_settings"
+
+  rules {
+    action = "set_cache_settings"
+
+    action_parameters {
+      respect_strong_etags = true
+    }
+
+    expression = "(http.host in {\"files.acearchive.lgbt\" \"api.acearchive.lgbt\"})"
+    description = "Enable Strong ETags"
+    enabled = true
+  }
+}
