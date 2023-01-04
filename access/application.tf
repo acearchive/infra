e30e9cc2337b72a60ae0082761174e3db31d7db8
Frawley <frawley@duck.com>
2023-01-04 17:41:32 -0500

    Relax session duration for Cloudflare Access
    There's nothing sensitive being guarded here—the only reason why these pages are
    access-controlled is to discourage users from accidentally visiting unreleased
    builds of the sites.

--- access/application.tf
+++ access/application.tf
@@ -2,16 +2,19 @@ resource "cloudflare_access_application" "acearchive" {
   account_id = var.cloudflare_account_id
   name = "acearchive-lgbt"
   domain = "*.acearchive-lgbt.pages.dev"
+  session_duration = "168h"
 }
 
 resource "cloudflare_access_application" "hha" {
   account_id = var.cloudflare_account_id
   name = "hha-archive"
   domain = "*.hha-archive.pages.dev"
+  session_duration = "168h"
 }
 
 resource "cloudflare_access_application" "nothingradical" {
   account_id = var.cloudflare_account_id
   name = "nothingradical-blog"
   domain = "*.nothingradical-blog.pages.dev"
+  session_duration = "168h"
 }
