4dc9a9e6d86cdbf813ae3ce9ed5538c7444cf0ad
Frawley <frawley@duck.com>
2023-10-05 18:36:42 -0400

    Add separate R2 bucket for non-prod use

--- acearchive/r2.tf
+++ acearchive/r2.tf
@@ -2,6 +2,10 @@ resource "aws_s3_bucket" "artifacts" {
   bucket = "acearchive-lgbt-artifacts"
 }
 
+resource "aws_s3_bucket" "artifacts_dev" {
+  bucket = "acearchive-lgbt-artifacts-dev"
+}
+
 resource "aws_s3_bucket" "staging" {
   bucket = "acearchive-lgbt-staging"
 }
