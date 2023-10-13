1789482419210132da3385e8f029cf72b07f35cd
Frawley <frawley@duck.com>
2023-10-13 08:40:00 -0400

    Reformat Terraform code

--- access/group.tf
+++ access/group.tf
@@ -1,6 +1,6 @@
 resource "cloudflare_access_group" "acearchive" {
   account_id = var.cloudflare_account_id
-  name = "Ace Archive Team"
+  name       = "Ace Archive Team"
 
   include {
     email = var.acearchive_group_emails
@@ -9,7 +9,7 @@ resource "cloudflare_access_group" "acearchive" {
 
 resource "cloudflare_access_group" "nothingradical" {
   account_id = var.cloudflare_account_id
-  name = "Nothing Radical Team"
+  name       = "Nothing Radical Team"
 
   include {
     email = var.nothingradical_group_emails
--- access/variables.tf
+++ access/variables.tf
@@ -1,20 +1,20 @@
 variable "cloudflare_api_token" {
-  type = string
+  type        = string
   description = "Cloudflare API token"
-  sensitive = true
+  sensitive   = true
 }
 
 variable "cloudflare_account_id" {
-  type = string
+  type        = string
   description = "Cloudflare account ID"
 }
 
 variable "acearchive_group_emails" {
-  type = list(string)
+  type        = list(string)
   description = "Email addresses for the Ace Archive group"
 }
 
 variable "nothingradical_group_emails" {
-  type = list(string)
+  type        = list(string)
   description = "Email addresses for the Nothing Radical group"
 }
