ccea5289c4fa26b1d7f8fffb3ddc8f1564830ec1
Frawley <frawley@duck.com>
2025-02-09 00:33:43 -0500

    Add R2 buckets for DB backups

--- acearchive/r2.tf
+++ acearchive/r2.tf
@@ -6,6 +6,14 @@ resource "aws_s3_bucket" "artifacts_dev" {
   bucket = "acearchive-lgbt-artifacts-dev"
 }
 
+resource "aws_s3_bucket" "db_backups" {
+  bucket = "acearchive-lgbt-db-backups"
+}
+
+resource "aws_s3_bucket" "db_backups_dev" {
+  bucket = "acearchive-lgbt-db-backups-dev"
+}
+
 # The purpose of this bucket is to give us (me) a place to manually upload files
 # that don't have a direct-download link on the web. For example, if I write a
 # transcript, I can upload it to this bucket using the Cloudflare dashboard and
